f5625315dff0bcffc41508b67d882f9269114950
Asteises <p.nikitaev@gmail.com>
2022-11-01 20:40:58 +0300

    add-docker

---
+++ shareit-gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -0,0 +1,29 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.user.dto.UserDto;
+
+@Service
+public class UserClient extends BaseClient {
+    private static final String API_PREFIX = "/users";
+
+    @Autowired
+    public UserClient(RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:9090" + API_PREFIX))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> createUser(UserDto userDto) {
+        return post("", userDto);
+    }
+}
---
+++ shareit-gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,51 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+
+    private final UserClient userClient;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
+        return userClient.createUser(userDto);
+    }
+
+//    @PatchMapping("/{userId}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable long userId) {
+//        return userService.updateUser(userDto, userId);
+//    }
+//
+//    @DeleteMapping("/{userId}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public void deleteUser(@PathVariable long userId) {
+//        userService.deleteUser(userId);
+//    }
+//
+//    @GetMapping
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<UserDto> findAllUsers() {
+//        return userService.findAllUsers();
+//    }
+//
+//    @GetMapping("/{userId}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public UserDto findUserById(@PathVariable long userId) {
+//        return userService.findUserById(userId);
+//    }
+}
+
